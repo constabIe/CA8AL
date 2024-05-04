76ecdfa0b2cda8d7687c4c31588b1f38c3c7ad09
constabIe <aiavkhadiev@edu.hse.ru>
2024-05-04 12:40:25 +0300

    01-1-Чёрное и кругло: sample №01

--- Contest_01/01/_asm/slv.asm
+++ Contest_01/01/_asm/slv.asm
@@ -5,3 +5,73 @@ extern io_get_char, io_get_string
 
 extern io_print_dec, io_print_udec, io_print_hex 
 extern io_print_char, o_print_string, io_newline
+
+; S_x = t * (vx + ax_half * t)
+; S_y = t * (vy + ay_half * t)
+
+section .text
+global main
+main:
+	; get input
+	call io_get_dec
+	mov dword [v], eax
+
+	call io_get_dec
+	mov dword [v + 1], eax
+
+	call io_get_dec
+	mov dword [a_half], eax
+
+	call io_get_dec
+	mov dword [a_half + 1], eax
+
+	call io_get_dec
+	mov dword [t], eax
+
+	; interim calculations
+	mov eax, [a_half]
+	mov ecx, [t]
+	mul ecx
+
+	add eax, [v]
+
+	mul ecx
+
+	mov [S], eax
+
+	mov eax, [a_half + 1]
+	mov ecx, [t]
+	mul ecx
+
+	add eax, [v + 1]
+
+	mul ecx
+
+	mov [S + 1], eax
+
+	; result output
+	mov eax, [S]
+	call io_print_dec
+
+	mov eax, space
+	call io_print_char
+
+	mov eax, [S + 1]
+	call io_print_dec
+
+	xor eax, eax
+	ret 0
+
+
+section .bss
+	; R \in \mathbb{R} ^ 2
+	v:			resd 2
+	a_half:		resd 2
+	t:			resd 1
+	S:			resd 2
+
+
+section .rodata
+	space: 		db ` `
+
+
--- Contest_01/01/_c/slv.c
+++ Contest_01/01/_c/slv.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <limits.h>
+
+
+int main (void) 
+{
+	// S = v_0 * t + ( a * t ^ 2 ) / 2
+	// input: vx, vy, ax / 2, ay / 2, t
+
+	int vx, vy, ax_half, ay_half, t;
+
+	scanf("%d %d %d %d %d", &vx, &vy, &ax_half, &ay_half, &t);
+
+	int S_x, S_y;
+
+	S_x = t * (vx + ax_half * t);
+	S_y = t * (vy + ay_half * t);
+
+	printf("(S_x, S_y): (%d, %d)", S_x, S_y);
+
+	return 0;
+}
