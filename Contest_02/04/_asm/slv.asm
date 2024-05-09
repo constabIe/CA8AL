69022e3394be591e123184a7818e609cef366d33
constabIe <aiavkhadiev@edu.hse.ru>
2024-05-09 16:38:26 +0300

    other

---
+++ Contest_02/04/_asm/slv.asm
@@ -0,0 +1,9 @@
+bits 32
+
+extern io_get_dec, io_get_udec, io_get_hex 
+extern io_get_char, io_get_string
+
+extern io_print_dec, io_print_udec, io_print_hex 
+extern io_print_char, o_print_string, io_newline
+
+
---
+++ Contest_02/04/_c/slv.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+
+int main(void) 
+{
+	uint32_t n;
+	scanf("%d", &n);
+
+	
+}
