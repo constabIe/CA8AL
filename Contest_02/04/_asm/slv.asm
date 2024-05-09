7fac029f5d6bce08ae25e6566adb9626d536d1e6
constabIe <aiavkhadiev@edu.hse.ru>
2024-05-09 17:39:22 +0300

    Задача 02-4: Повёрнутая бесконечность. Sample №1

--- Contest_02/04/_asm/slv.asm
+++ Contest_02/04/_asm/slv.asm
@@ -1,9 +1,45 @@
 bits 32
 
-extern io_get_dec, io_get_udec, io_get_hex 
-extern io_get_char, io_get_string
+%include "io.inc"
 
-extern io_print_dec, io_print_udec, io_print_hex 
-extern io_print_char, o_print_string, io_newline
+section .text
+global main
+main:
+	; get input
+	GET_UDEC 4, eax
+	; mov [n], eax
 
+	; I am going to divide [n] to 8 until quotient -- result of division -- more than 8. I.e. the standard steps for converting 
+	; from the 10 to 8 number system 
+
+	mov ebx, 8
+	mov ecx, 0
+
+	jmp cycle
+cycle:
+	inc ecx
+
+	cdq
+	div ebx
+
+	cmp edx, ebx
+	jns if 		; quotient < 8
+	jns else
+
+if: 
+	push edx
+	jmp cycle
+
+else:
+	push eax
+	jmp output_loop
+
+output_loop:
+	pop eax
+	PRINT_UDEC 4, eax
+
+	loop output_loop
+
+; section .bss
+; 	n:	resd	1
 
