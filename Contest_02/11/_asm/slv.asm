802c91e071efec4c9f259fc35cfb762bbe7b650b
constabIe <aiavkhadiev@edu.hse.ru>
2024-05-10 23:07:47 +0300

    02-11: НОД. Sample №20

--- Contest_02/11/_asm/slv.asm
+++ Contest_02/11/_asm/slv.asm
@@ -2,31 +2,14 @@ bits 32
 
 %include "io.inc"
 
-MAX_LIMIT 	equ	1000000000
-MIN_LIMIT 	equ	2
-
 section .text
 global main
 main:
-	; get and verify input
-	GET_UDEC 4, eax
-
-	cmp eax, MIN_LIMIT
-	js RangeExceptionCondition
-
-	cmp eax, MAX_LIMIT
-	jns RangeExceptionCondition
-
+	; input
+	GET_DEC 4, eax
 	mov [a], eax
 
-	GET_UDEC 4, ebx
-
-	cmp ebx, MIN_LIMIT
-	js RangeExceptionCondition
-
-	cmp ebx, MAX_LIMIT
-	jns RangeExceptionCondition
-
+	GET_DEC 4, ebx
 	mov [b], ebx
 
 	; a = max(a, b) 
@@ -34,13 +17,7 @@ main:
 	js if_1
 	jns continue_main
 
-	PRINT_UDEC 4, [a]
-	NEWLINE
-	PRINT_UDEC 4, [b]
-	NEWLINE
-
-	; b > a
-if_1:				
+if_1: ; (b > a)
 	xchg eax, ebx
 	mov [a], eax
 	mov [b], ebx
@@ -69,8 +46,7 @@ gcd_cycle:
 
 	jmp gcd_cycle
 
-	; remainder == 0
-if_2:
+if_2: ; (remainder == 0)
 	jmp exit_program
 
 exit_program:
@@ -80,18 +56,7 @@ exit_program:
 	xor eax, eax
 	ret
 
-RangeExceptionCondition:
-	PRINT_STRING RangeExceptionMessage
-	NEWLINE
-
-	xor eax, eax
-	int 0x0A
-
-
 section .bss
 	a:	resd	1
 	b:	resd	1
-
-section .rodata
-	RangeExceptionMessage	db	`Input data is out of range`, 0
 
