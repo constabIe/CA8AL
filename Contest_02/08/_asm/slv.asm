abf9b13af8e97386f2297b5e654ce5240067109f
constabIe <aiavkhadiev@edu.hse.ru>
2024-05-12 20:08:32 +0300

    02-8: Умножение. Sample #1

--- Contest_02/08/_asm/slv.asm
+++ Contest_02/08/_asm/slv.asm
@@ -1,7 +1,204 @@
 bits 32
 
-extern io_get_dec, io_get_udec, io_get_hex 
-extern io_get_char, io_get_string
+%inclue "io.inc"
 
-extern io_print_dec, io_print_udec, io_print_hex 
-extern io_print_char, o_print_string, io_newline
+N equ 3
+M equ 4
+k equ 5
+
+MAX_SIZE		equ	100
+MIN_SIZE		equ	1
+MAX_CELL_VAL	equ	1000
+MIN_CELL_VAL	equ	-1000	
+INT32_SIZE		equ 4
+
+
+global	mscanf
+
+%define	src_size_y			dword [ebp + 16]
+%define src_size_x			dword [ebp + 12]
+%define matrix_base			dword [ebp +  8]
+
+mscanf:
+	push 	ebp
+	mov 	esp, ebp
+
+	push 	eax
+	push 	ebx
+	push 	ecx
+	push 	edx
+
+    xor 	eax, eax
+    mov 	ecx, matrix_base
+
+.rows_loop:
+ 	cmp 	eax, src_size_y
+ 	jae 	mscanf.rows_loop_exit 
+
+ 	xor 	ebx, ebx
+
+ 	.cols_loop:
+ 		cmp     ebx, src_size_x
+        jae     mscanf.cols_loop_exit
+
+        GET_DEC	4, edx
+
+        cmp 	edx, MAX_CELL_VAL
+        ja 		RangeExceptionCondition
+
+        cmp 	edx, MIN_CELL_VAL
+        jb 		RangeExceptionCondition
+
+        mov 	[ecx], edx
+
+        add 	ecx, INT32_SIZE
+
+        inc 	ebx
+        jmp 	mscanf.cols_loop
+
+    .cols_loop_exit:
+    	inc 	eax
+    	jmp 	mscanf.rows_loop
+
+.rows_loop_exit:
+	pop 	edx 	 
+	pop 	ecx 	 	
+	pop 	ebx 	 	
+	pop 	eax 
+
+	mov     esp, ebp
+    pop     ebp	
+
+	ret  
+
+global mprintf
+
+%define	src_size_y			dword [ebp + 16]
+%define src_size_x			dword [ebp + 12]
+%define matrix_base			dword [ebp +  8]
+
+mprintf:
+	push 	ebp
+	mov 	esp, ebp
+
+	push 	eax
+	push 	ebx
+	push 	ecx
+
+    xor 	eax, eax
+    mov 	ecx, matrix_base
+
+ .rows_loop:
+ 	cmp 	eax, src_size_y
+ 	jae 	mprintf.rows_loop_exit 
+
+ 	xor 	ebx, ebx
+
+ 	.cols_loop:
+ 		cmp     ebx, src_size_x
+        jae     mprintf.cols_loop_exit
+
+        PRINT_DEC	4, [ecx]
+
+        add 	ecx, INT32_SIZE
+
+        inc 	ebx
+        jmp 	mprintf.cols_loop
+
+    .cols_loop_exit:
+    	inc 	eax
+    	jmp 	mprintf.rows_loop
+
+.rows_loop_exit:	 
+	pop 	ecx 	 	
+	pop 	ebx 	 	
+	pop 	eax 
+
+	mov     esp, ebp
+    pop     ebp	
+
+	ret  
+
+global matrix_product_operation
+%define dest_size_x
+%define matrix_operand_2_base
+%define matrix_operand_1_base
+
+
+
+section .text
+global main
+main:
+	GET_DEC 4, eax
+
+	cmp 	eax, MAX_SIZE
+	ja RangeExceptionCondition
+
+	cmp 	eax, MIN_SIZE
+	jb 		RangeExceptionCondition
+
+	mov 	[n], eax
+
+	GET_DEC 4, eax
+
+	cmp 	eax, MAX_SIZE
+	ja 		RangeExceptionCondition
+
+	cmp 	eax, MIN_SIZE
+	jb 		RangeExceptionCondition
+
+	mov 	[m], eax
+
+	GET_DEC 4, eax
+
+	cmp 	eax, MAX_SIZE
+	ja 		RangeExceptionCondition
+
+	cmp 	eax, MIN_SIZE
+	jb 		RangeExceptionCondition
+
+	mov 	[k], eax
+
+	mov 	ecx, N * M
+
+	mov 	eax, operand_matrix_1
+	mov 	ecx, N * M
+
+	call 	scanf_matrix
+
+	mov 	eax, operand_matrix_2
+	mov 	ecx, M * K
+	xor 	edx, edx
+
+	mov 	eax, M
+	mov 	ebx, N
+	imul 	ebx
+
+	mov 	[ebp + 8], operand_matrix_1
+	mov 	[ebp + 12], eax
+	call 	mscanf
+
+
+
+
+
+	
+	
+
+RangeExceptionCondition:
+	PRINT_STRING RangeExceptionMessage
+	NEWLINE
+
+	xor eax, eax
+	int 0x0A
+
+section .bss
+	n: 					resd	1
+	m: 					resd 	1
+	k:					resd	1
+
+	operand_matrix_1:	resd 	N * M
+	operand_matrix_2:	resd 	M * K
+
+section .rodata
+	RangeExceptionMessage	db `Input data is out of range`, 0
--- Contest_02/08/_c/slv.c
+++ Contest_02/08/_c/slv.c
@@ -1,8 +1,135 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <limits.h>
+#include <stdlib.h>
+
+#define EXIT_FAILURE 1
+
+#define BRED  "\033[1;31m"
+#define RESET "\033[0m"
+
+#define VERIFY_CONTRACT(contract, format, ...) \
+    do { \
+        if (!(contract)) { \
+            printf((format), ##__VA_ARGS__ ); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while (0)
+
+
+typedef struct 
+{ 
+	int **data;
+ 	int rows_q; 
+ 	int cols_q; 
+
+} Matrix_t;	
+
+Matrix_t* allocate_matrix(const int rows_q, const int cols_q); 
+void deallocate_matrix(Matrix_t *matrix); 
+
+void matrix_scanf(Matrix_t *dest_matrix); 
+void matrix_printf(Matrix_t *source_matrix);
+
+Matrix_t *matrix_product_operation(const Matrix_t *opearnd_1, const Matrix_t *opearnd_2); 	
 
 int main(void) 
 {
-	
+	int n, m, k;
+	VERIFY_CONTRACT(scanf("%d %d %d", &n, &m, &k) == 3, "\nInvalid data input\n");
+
+	Matrix_t *matrix_operand_1 = allocate_matrix(n, m);
+	Matrix_t *matrix_operand_2 = allocate_matrix(m, k);
+
+	matrix_scanf(matrix_operand_1);
+	matrix_scanf(matrix_operand_2);	
+
+	Matrix_t *matrixes_product_result = matrix_product_operation(matrix_operand_1, matrix_operand_2);
+
+	matrix_printf(matrixes_product_result);
+
+	deallocate_matrix(matrix_operand_1);
+	deallocate_matrix(matrix_operand_2);
+
+	return 0;
+}
+
+Matrix_t* allocate_matrix(const int rows_q, const int cols_q) 
+{
+	Matrix_t *matrix = (Matrix_t *) malloc(sizeof(Matrix_t));
+
+	matrix->rows_q = rows_q;
+	matrix->cols_q = cols_q;
+
+	matrix->data = (int **) malloc(matrix->rows_q * sizeof(int *));
+	VERIFY_CONTRACT(matrix->data != NULL, "\nUnable to allocate memory\n");
+
+	for (int i = 0; i < matrix->rows_q; ++i)
+	{
+		matrix->data[i] = (int *) malloc(matrix->cols_q * sizeof(int));
+		VERIFY_CONTRACT(matrix->data[i] != NULL, "\nUnable to allocate memory\n");
+	}
+
+	return matrix;
+}
+
+void deallocate_matrix(Matrix_t *matrix) 
+{
+	for (int i = 0; i < matrix->rows_q; ++i)
+	{
+		free(matrix->data[i]);
+	}
+
+	free(matrix->data);
+	free(matrix);
+}
+
+void matrix_scanf(Matrix_t *dest_matrix) 
+{
+	for (int i = 0; i < dest_matrix->rows_q; ++i)
+	{
+		for (int j = 0; j < dest_matrix->cols_q; ++j)
+		{
+			VERIFY_CONTRACT(scanf("%d", &dest_matrix->data[i][j]) == 1, "\nInvalid data input"\n);
+		}
+	}
+}
+
+void matrix_printf(Matrix_t *source_matrix) 
+{
+	printf("\n\n");
+
+	for (int i = 0; i < source_matrix->rows_q; ++i)
+	{
+		for (int j = 0; j < source_matrix->cols_q; ++j)
+		{
+			printf(" %d", source_matrix->data[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+Matrix_t *matrix_product_operation(const Matrix_t *opearnd_1, const Matrix_t *opearnd_2) 
+{
+	VERIFY_CONTRACT(opearnd_1->cols_q == opearnd_2->rows_q, "\nUnable to perfom operation. Invalid matrix size\n");
+
+	int rows_q_result_matrix = opearnd_1->rows_q;
+	int cols_q_result_matrix = opearnd_2->cols_q;
+
+	Matrix_t *result_matrix = allocate_matrix(rows_q_result_matrix, cols_q_result_matrix);
+
+	int cell_itterations_q = opearnd_1->cols_q & opearnd_2->rows_q;
+
+	for (int i = 0; i < result_matrix->rows_q; ++i)
+	{	
+		for (int j = 0; j < result_matrix->cols_q; ++j)
+		{
+			for (int w = cell_itterations_q - 1; w >= 0; --w)
+			{
+				result_matrix->data[i][j] += opearnd_1->data[i][w] * opearnd_2->data[w][j];
+			}
+		}
+	}
+
+	return result_matrix;
 }
