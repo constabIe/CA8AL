842943c691467d12670eb9c297f6f89b8690e19a
constabIe <aiavkhadiev@edu.hse.ru>
2024-05-16 13:56:35 +0300

    03-4: Разворот половины последовательности. Sample #1

--- Contest_03/04/_asm/slv.asm
+++ Contest_03/04/_asm/slv.asm
@@ -1,3 +1,65 @@
 bits 32
 
-%inclue "io.inc"
+%include "io.inc"
+
+section .text
+global reverse_half
+reverse_half:
+	push 	ebp
+	mov 	ebp, esp
+
+	push 	eax
+	push 	ecx
+
+	xor 	ecx, ecx
+	.while:
+		GET_DEC 4, eax
+
+		test 	ecx, ecx
+		jnp 	.out_odd
+		jp 		.in_even
+		jmp 	.exit_while
+
+		.out_odd: 	
+			PRINT_DEC 4, eax
+			PRINT_CHAR ` `
+
+			jmp 	.while
+
+		.in_even:
+			inc 	ecx
+			push 	eax
+
+			jmp 	.while
+
+	.exit_while:
+		jmp .out_even_loop
+
+	.out_even_loop:
+		pop  	eax
+
+		PRINT_DEC 4, eax
+		PRINT_CHAR ` `
+
+		loop 	.out_even_loop
+
+		jmp  	.exit_function
+
+	.exit_function:
+		pop  	ecx
+		pop  	eax
+
+		mov  	esp, ebp
+		pop  	ebp
+
+		ret
+
+global main:
+main:
+	call 	reverse_half
+
+	xor  	eax, eax
+	ret
+
+
+
