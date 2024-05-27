18051c4af86c5e82adb77a4dedc94db611f33225
constabIe <aiavkhadiev@edu.hse.ru>
2024-05-27 20:54:37 +0300

    04-9: Матрица с максимальным следом. Sample #50

--- Contest_03.2/09/_asm/slv.asm
+++ Contest_03.2/09/_asm/slv.asm
@@ -5,6 +5,8 @@ extern	scanf, printf
 
 section .text
 
+; -----------define_macro-----------
+
 %macro ALIGN_STACK 1.nolist
 	sub		esp, %1
 	and		esp, 0xfffffff0
@@ -15,221 +17,201 @@ section .text
 	add		esp, %1
 %endmacro
 
-; ==================================
+%macro FUNCTION_PROLOGUE 1.nolist
+	enter	%1, 0
+	and 	esp, 0xfffffff0
+%endmacro
+
+%macro FUNCTION_EPILOGUE 1.nolist
+	add		esp, %1
+	leave
+%endmacro	
+
+; ----------implement_main----------
+
 global main
 main:
-	enter 0, 0
-
-	ALIGN_STACK 4
-	push	w
-	call	printf
-	UNALIGN_STACK 4
+	FUNCTION_PROLOGUE 0
 
 	ALIGN_STACK 8
 	push	n
 	push	i_format
-	call 	scanf
-	UNALIGN_STACK 8	
-
-	ALIGN_STACK 4
-	push	w
-	call	printf
-	UNALIGN_STACK 4
+	call	scanf
+	UNALIGN_STACK 8
 
 	ALIGN_STACK 4
 	push	dword [n]
 	call	allocate_matrix
-	UNALIGN_STACK 4	
+	UNALIGN_STACK 4
 
-	mov	[matrix_base], eax
+	mov		[matrix_ptr], eax
 
 	ALIGN_STACK 8
 	push	dword [n]
-	push	dword [matrix_base]
+	push	dword [matrix_ptr]
 	call	scanf_matrix
-	UNALIGN_STACK 8	
+	UNALIGN_STACK 8
 
 	ALIGN_STACK 8
 	push	dword [n]
-	push	dword [matrix_base]
+	push	dword [matrix_ptr]
 	call	printf_matrix
 	UNALIGN_STACK 8	
 
 	ALIGN_STACK 4
-	push	dword [matrix_base]
+	push	dword [matrix_ptr]
 	call	deallocate_matrix
 	UNALIGN_STACK 4	
 
-	leave
+	FUNCTION_EPILOGUE 0
 
-	xor		eax, eax
 	ret
 
-; ==================================
+; --------functions_implementation--------
 
-%define	matrix_order 	dword [ebp + 8]
+%define matrix_order 	dword [ebp + 8]
 
 global allocate_matrix
 allocate_matrix:
-	enter 	0, 0
+	FUNCTION_PROLOGUE 0
 
 	push	ebx
-	push	ecx
 
 	mov		eax, matrix_order
 	mul		matrix_order
 
-	mov		ebx, DWORD_SIZE	
+	mov		ebx, DWORD_SIZE
 	mul		ebx
 
-	ALIGN_STACK 4
-	push	w
-	call	printf
-	UNALIGN_STACK 4
-
-	ALIGN_STACK 4
+	ALIGN_STACK	4
 	push	eax
-	call	malloc
+	call 	malloc
 	UNALIGN_STACK 4
 
-	ALIGN_STACK 4
-	push	w
-	call	printf
-	UNALIGN_STACK 4
-
-	pop		ecx
 	pop		ebx
 
-	leave
+	FUNCTION_EPILOGUE 0
 
 	ret
 
 %undef	matrix_order
 
-%define	matrix_ptr 	dword [ebp + 8]
+; ---------------------------------------
+
+%define	matrix_base		dword [ebp + 8]
 
 global deallocate_matrix
 deallocate_matrix:
-	enter 	0, 0
+	FUNCTION_PROLOGUE 0
 
-	ALIGN_STACK 4
-	push	matrix_ptr
-	call	free
+	ALIGN_STACK	4
+	push	matrix_base
+	call 	free
 	UNALIGN_STACK 4
 
-	leave
+	FUNCTION_EPILOGUE 0
 
-	ret
+	ret	
 
-%undef	matrix_ptr
+%undef	matrix_base
+
+; ---------------------------------------
 
 %define	matrix_order	dword [ebp + 12]
-%define	matrix_ptr		dword [ebp +  8]
+%define	matrix_base 	dword [ebp +  8]
+%define	iterator		dword [ebp -  4]
 
 global scanf_matrix
 scanf_matrix:
-	enter 0, 0
+	FUNCTION_PROLOGUE 4
 
-	push	eax
 	push	ebx
-	push	ecx
-
-	mov		eax, matrix_order
-	imul	matrix_order
-
-	mov		ecx, eax
-	mov		ebx, matrix_ptr
-
-	; ALIGN_STACK 8
-	; push	ecx
-	; push 	o_format
-	; call	printf
-	; UNALIGN_STACK 8
-
-	.input_loop:
-		ALIGN_STACK 4
-		push	w
-		call	printf
-		UNALIGN_STACK 4
-
-		cmp		ecx, 0
-		jbe		scanf_matrix.exit_function
-
-		ALIGN_STACK 8
-		push	ebx
-		push	i_format 
-		call	scanf
-		UNALIGN_STACK 8
-
-		dec		ecx
-		add		ebx, 4
-
-		jmp  	.input_loop
-
-.exit_function:
-	pop		ecx
-	pop		ebx
-	pop		eax
-
-	leave
-
-	ret
-
-%undef	matrix_order
-%undef	matrix_ptr
-
-%define	matrix_order	dword [ebp + 12]
-%define	matrix_ptr		dword [ebp +  8]
-
-global printf_matrix
-printf_matrix:
-	enter 0, 0
-
-	push	eax
-	push	ebx
-	push	ecx
 
 	mov		eax, matrix_order
 	mul		matrix_order
+	mul		matrix_order
 
-	mov		ecx, eax
-	mov		ebx, matrix_ptr
+	mov		iterator, eax
+	mov		ebx, matrix_base
+
+	.get_input_loop:
+		cmp		iterator, 0
+		jle		scanf_matrix.exit_function
+
+		ALIGN_STACK 8
+		push	ebx
+		push	i_format
+		call 	scanf
+		UNALIGN_STACK 8
+
+		dec		iterator
+		add		ebx, 4
+
+		jmp		.get_input_loop
+
+.exit_function:
+	pop		ebx
+
+	FUNCTION_EPILOGUE 4
+
+	ret		
+
+%undef	matrix_order
+%undef	matrix_base
+%undef	iterator
+
+; ---------------------------------------
+
+%define	matrix_order	dword [ebp + 12]
+%define	matrix_base 	dword [ebp +  8]
+%define	iterator		dword [ebp -  4]
+
+global printf_matrix
+printf_matrix:
+	FUNCTION_PROLOGUE 4
+
+	push	ebx
+
+	mov		eax, matrix_order
+	mul		matrix_order
+	mul		matrix_order
+
+	mov		iterator, eax
+	mov		ebx, matrix_base
 
 	.output_loop:
-		cmp		ecx, 0
-		jbe		printf_matrix.exit_function
+		cmp		iterator, 0
+		jle		printf_matrix.exit_function
 
 		ALIGN_STACK 8
 		push	dword [ebx]
 		push	o_format
-		call	scanf
+		call 	scanf
 		UNALIGN_STACK 8
 
-		dec		ecx
+		dec		iterator
 		add		ebx, 4
 
-		jmp  	.output_loop
+		jmp		.output_loop
 
 .exit_function:
-	pop		ecx
 	pop		ebx
-	pop		eax
 
-	leave
+	FUNCTION_EPILOGUE 4
 
-	ret
+	ret		
 
 %undef	matrix_order
-%undef	matrix_ptr
+%undef	matrix_base
+%undef	iterator
 
-section .data
-	DWORD_SIZE	equ 	4
-
-	i_format	db		`%d`, 0
-	o_format	db		`_%d_`, 0
-	newline		db		`\n`, 0
-	w			db		`w`, 0
+section	.data
+	DWORD_SIZE	equ		4
+	i_format	db		"%d", 0
+	o_format	db		"%d "
 
 section .bss
-	n			resd	1
-	matrix_base	resd	1	
+	n 		resd	1
+	matrix_ptr 	resd	1
+
+
