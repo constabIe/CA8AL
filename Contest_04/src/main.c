7ad10fc6547d2be8c69e8b2c6b6197e412126654
constabIe <aiavkhadiev@edu.hse.ru>
2024-06-18 02:22:44 +0300

    f_subs prototype

--- Contest_04/src/main.c
+++ Contest_04/src/main.c
@@ -17,7 +17,7 @@ int main(void) {
     // printf("%lu\n" , sizeof(func->obj_rpn->rpn));
 
 // //    set_variable(func, "y");
-   printf("%lf\n\n", func->obj_rpn->rpn[0]-obj->operand->obj);
+   printf("%lf\n\n", func->obj_rpn->rpn[0]->obj->operand->obj);
 //    printf("%u\n", func->obj_rpn->rpn[1]->type);
 //    printf("%u\n", func->obj_rpn->rpn[2]->type);
 
